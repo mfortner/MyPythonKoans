301e5526d4b4349e55ec8a5a2f710246ddcb9c03
Mike Fortner <michaeljamesfortner@gmail.com>
2012-10-02 18:19:30 -0700

    real initial commit

---
+++ python2/koans/triangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Triangle Project Code.
+
+
+# triangle(a, b, c) analyzes the lengths of the sides of a triangle
+# (represented by a, b and c) and returns the type of triangle.
+#
+# It returns:
+#   'equilateral'  if all sides are equal
+#   'isosceles'    if exactly 2 sides are equal
+#   'scalene'      if no sides are equal
+#
+# The tests for this method can be found in
+#   about_triangle_project.py
+# and
+#   about_triangle_project_2.py
+#
+def triangle(a, b, c):
+
+	if (a<=0 or b<=0 or c<=0):
+		raise TriangleError
+	if (a+b<=c or b+c<=a or c+a<=b):
+		raise TriangleError	
+
+	n = 0
+	if a==b: n+=1
+	if b==c: n+=1
+	if c==a: n+=1
+
+	if n>1: return 'equilateral'
+	elif n == 1: return 'isosceles'
+	else: return 'scalene'
+
+# Error class used in part 2.  No need to change this code.
+class TriangleError(StandardError):
+    pass
